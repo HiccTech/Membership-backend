6add0b82ca0cd11a1ac0b0c1c6403c5cca0463d9
Neo zhang <neo@hiccpet.com>
2025-09-04 17:28:53 +0800

    refactor: 优化

--- router/router.go
+++ router/router.go
@@ -11,7 +11,6 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
-	"fmt"
 	"hiccpet/service/config"
 	"log"
 )
@@ -39,14 +38,6 @@ func runStatic(r *gin.Engine) {
 }
 
 func SetupRouter() *gin.Engine {
-
-	// 初始化配置
-	config.LoadConfig()
-	fmt.Println("当前环境 =", config.Cfg.ShopEnv)
-	fmt.Println("店铺名称 =", config.Cfg.StoreName)
-	fmt.Println("token =", config.Cfg.AccessToken)
-	fmt.Println("数据库连接 =", config.Cfg.DbDSN)
-
 	// 连接 MySQL
 	dsn := config.Cfg.DbDSN
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -64,7 +55,6 @@ func SetupRouter() *gin.Engine {
 	r.POST("/register", func(c *gin.Context) { handler.Register(c, db) })
 	r.POST("/login", func(c *gin.Context) { handler.Login(c, db) })
 
-	// 定义 storefront 分组
 	storefront := r.Group("/storefront")
 	storefront.Use(middleware.ShopifySessionAuth())
 	{
@@ -93,14 +83,10 @@ func SetupRouter() *gin.Engine {
 		})
 	}
 
-	// 受保护接口
 	auth := r.Group("/api")
 	auth.Use(middleware.JWTAuthMiddleware())
 	{
 
-		auth.GET("/profile", func(c *gin.Context) {
-			c.JSON(200, gin.H{"message": "This is protected data"})
-		})
 		auth.POST("/addStore", func(c *gin.Context) { handler.AddStore(c, db) })
 		auth.GET("/getStores", func(c *gin.Context) { handler.GetStores(c, db) })
 	}
