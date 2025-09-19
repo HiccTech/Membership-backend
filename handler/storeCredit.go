d75921627706e0acdd4e79a040fc4b1fc02c1ea7
neo666 <neo@hiccpet.com>
2025-09-19 14:07:36 +0800

    feat: 优化GetStoreCreditBalance

--- handler/storeCredit.go
+++ handler/storeCredit.go
@@ -13,26 +13,29 @@ import (
 
 func GetStoreCreditBalance(c *gin.Context, db *gorm.DB) {
 	id := c.MustGet("shopifyClaims").(*middleware.ShopifyClaims).Sub
+	currencyCode, exists := c.GetQuery("currencyCode")
+	if !exists {
+		currencyCode = "SGD"
+	}
+
 	fmt.Println("Shopify customer ID:", id)
 	query := `#graphql
-		query GetCustomer($id: ID!){
+		query GetCustomer($id: ID!,$currencyCodeQuery: String!){
 			customer(id: $id) {
-			id
-			tags
-			storeCreditAccounts(first:3){
+			storeCreditAccounts(first:1, query: $currencyCodeQuery){
 				nodes {
 						balance {
 							amount
 							currencyCode
 						}
-						id
 					}
 				}
 			}
 		}`
 
 	resp, err := utils.CallShopifyGraphQL(query, map[string]interface{}{
-		"id": id,
+		"id":                id,
+		"currencyCodeQuery": fmt.Sprintf("currency_code:%s", currencyCode),
 	}, "")
 	if err != nil {
 		response.Error(c, http.StatusInternalServerError, err.Error())
